1cf486929fb046077fedb48820d1d2bd18b755c5
igrok1803444 <v618591@gmail.com>
2023-11-25 22:00:42 +0200

    end hw

--- src/4.ts
+++ src/4.ts
@@ -1,9 +1,57 @@
+
+class Key {
+    private signature: number = Math.random();
+
+    getSignature(): number {
+        return this.signature
+    }
+    
+
+}
+class Person {
+    
+    constructor( private key: Key) {
+      
+    }
+    getKey(): number{
+        return this.key.getSignature();
+    }
+}
+ 
+abstract class House {
+    protected door: boolean;
+    protected tenants: Person[]
+    constructor(protected key: Key) {
+         
+    }
+    comeIn(person: Person): void{
+        if (this.door) {
+            this.tenants.push()
+        }
+    }
+
+    abstract  OpenDoor(key: number): void
+}
+
+class MyHouse extends House{
+
+    constructor( key: Key) {
+        super(key)
+    }
+
+    OpenDoor( key: number): void {
+        if (key === this.key.getSignature()) {
+            this.door = true
+        }
+    }
+
+}
 const key = new Key();
 
 const house = new MyHouse(key);
 const person = new Person(key);
 
-house.openDoor(person.getKey());
+house.OpenDoor(person.getKey());
 
 house.comeIn(person);
 
